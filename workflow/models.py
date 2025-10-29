a7edd413b2dd500d45558299f9f63ed5f365c2f2
Manoj <manojleema12345@gmail.com>
2025-10-29 07:54:49 +0530

    first commit

---
+++ workflow/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+# Create your models here.
+
+class UdiFiaWorkflow(models.Model):
+    change_number = models.CharField(max_length=50)
+    udr_fia_number = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    region = models.CharField(max_length=50)
+    gtin_change = models.BooleanField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    product_type = models.CharField(max_length=50)
+    product_category_unit = models.CharField(max_length=50)
+    product_category_level = models.CharField(max_length=50)
+    gtin_evaluation =  models.BooleanField(null=True)
+    has_udi_health_impact = models.CharField(max_length=5)
+    has_impact_in_new_gtin = models.CharField(max_length=5)
+
+class ChangeCategories(models.Model):
+    name = models.CharField(max_length=50)
+    image = models.ImageField()
+
+class ChangesInvolved(models.Model):
+    workflow = models.ForeignKey(UdiFiaWorkflow, on_delete=models.CASCADE)
+    change_category = models.ForeignKey(ChangeCategories, on_delete=models.SET_NULL,null=True)
+    change_description = models.TextField(blank=True, null=True)
